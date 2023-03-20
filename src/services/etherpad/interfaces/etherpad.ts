d78da5f19efa11ea9fdae6d87311cc5904744d07
Alexandre Chau <dialexo@users.noreply.github.com>
2023-03-20 13:24:36 +0100

    fix: differentiate Etherpad result type from extra type, refactor H5P extra (#117)

--- src/services/etherpad/interfaces/etherpad.ts
+++ src/services/etherpad/interfaces/etherpad.ts
@@ -1,10 +1,19 @@
 import { ItemType } from '../../../constants';
 import { UnknownExtra } from '@/index';
 
+/**
+ * This type refers to the result sent by the server when querying
+ * the /view or /read endpoints of the etherpad plugin API!
+ */
 export type Etherpad = {
   padUrl: string;
 };
 
+export type EtherpadItemExtraProperties = {
+  padID: string;
+  groupID: string;
+};
+
 export interface EtherpadItemExtra extends UnknownExtra {
-  [ItemType.ETHERPAD]: Etherpad;
+  [ItemType.ETHERPAD]: EtherpadItemExtraProperties;
 }
--- src/services/h5p/interfaces/extra.ts
+++ src/services/h5p/interfaces/extra.ts
@@ -1,13 +1,15 @@
 import { ItemType } from '../../../constants';
 import { UnknownExtra } from '../../../interfaces';
 
+export type H5PItemExtraProperties = {
+  /** storage ID */
+  contentId: string;
+  /** relative path from root storage to the uploaded .h5p package */
+  h5pFilePath: string;
+  /** relative path from root storage to the assets folder */
+  contentFilePath: string;
+};
+
 export interface H5PItemExtra extends UnknownExtra {
-  [ItemType.H5P]: {
-    /** storage ID */
-    contentId: string;
-    /** relative path from root storage to the uploaded .h5p package */
-    h5pFilePath: string;
-    /** relative path from root storage to the assets folder */
-    contentFilePath: string;
-  };
+  [ItemType.H5P]: H5PItemExtraProperties;
 }
