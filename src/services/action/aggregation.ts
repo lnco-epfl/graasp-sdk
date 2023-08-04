b07d02af61cb69e770e1dbc5d24dbae0f94a45c2
Kim Lan Phan Hoang <pyphilia@gmail.com>
2023-08-04 10:18:43 +0200

    feat: agg enums (#222)
    * feat: add aggregation enums

---
+++ src/services/action/aggregation.ts
@@ -0,0 +1,35 @@
+export enum AggregateBy {
+  ActionCount = 'actionCount',
+  ActionLocation = 'actionLocation',
+  ActionType = 'actionType',
+  CreatedDay = 'createdDay',
+  CreatedDayOfWeek = 'createdDayOfWeek',
+  CreatedTimeOfDay = 'createdTimeOfDay',
+  ItemId = 'itemId',
+}
+export enum AggregateFunction {
+  Avg = 'avg',
+  Count = 'count',
+  Sum = 'sum',
+}
+
+export enum AggregateMetric {
+  ActionCount = 'actionCount',
+  ActionLocation = 'actionLocation',
+  ActionType = 'actionType',
+  CreatedDay = 'createdDay',
+  CreatedDayOfWeek = 'createdDayOfWeek',
+  CreatedTimeOfDay = 'createdTimeOfDay',
+  ItemId = 'itemId',
+  User = 'user',
+}
+
+export enum CountGroupBy {
+  ActionType = 'actionType',
+  ActionLocation = 'actionLocation',
+  CreatedDay = 'createdDay',
+  CreatedDayOfWeek = 'createdDayOfWeek',
+  CreatedTimeOfDay = 'createdTimeOfDay',
+  ItemId = 'itemId',
+  User = 'user',
+}
