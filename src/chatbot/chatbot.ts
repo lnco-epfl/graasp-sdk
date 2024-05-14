524f692e2c741482b4ac23b7a364850152756dc1
Basile Spaenlehauer <spaenleh@gmail.com>
2024-05-14 14:58:08 +0200

    fix: add GPT4O option to chatbot model enum (#522)

--- src/chatbot/chatbot.ts
+++ src/chatbot/chatbot.ts
@@ -5,9 +5,27 @@ export enum ChatbotRole {
 }
 
 export enum GPTVersion {
+  /**
+   * Default GPT model used. Has a 16K context window.
+   * Costs the least of all models with ok precision and speed
+   */
   GPT_3_5_TURBO = 'gpt-3.5-turbo-0125',
+  /**
+   * First generation GPT 4 model. Has an 8K token context window.
+   * Costs a lot and is not very fast.
+   */
   GPT_4 = 'gpt-4',
+  /**
+   * Fast version of GPT 4. Has 128K token context window.
+   * Costs less than GPT 4 but more thn 3.5-turbo.
+   * Best for real time conversations.
+   */
   GPT_4_TURBO = 'gpt-4-turbo',
+  /**
+   * New and improved version of GPT 4. Has a 128K token context window.
+   * Has better capabilities than GPT 4 for a 6th of the price.
+   */
+  GPT_4_O = 'gpt-4o',
 }
 
 export type ChatBotMessage = {
