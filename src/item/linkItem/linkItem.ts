79f6d050c655ec9fab1bd7d6928427cdab736c0b
Basile Spaenlehauer <spaenleh@gmail.com>
2024-05-22 14:01:29 +0200

    fix: add description on link extra properties (#527)
    * fix: add description on link extra properties
    
    * chore: update documentation
    
    Co-authored-by: Kim Lan Phan Hoang <pyphilia@gmail.com>
    
    ---------
    
    Co-authored-by: Kim Lan Phan Hoang <pyphilia@gmail.com>

--- src/item/linkItem/linkItem.ts
+++ src/item/linkItem/linkItem.ts
@@ -12,10 +12,32 @@ export type LinkItemType<S = ItemSettings> = {
  * Link Extra
  */
 export type LinkItemExtraProperties = {
-  thumbnails?: string[];
-  html?: string;
+  /**
+   * Link URL
+   */
   url: string;
+  /**
+   * Contains automatic thumbnails for the page, usually better used for large cards or banners
+   */
+  thumbnails?: string[];
+  /**
+   * Contains automatic icons for the page, usually better used as thumbnails as they
+   * are commonly extracted from the favicon of the site
+   */
   icons?: string[];
+  /**
+   * HTML integration code for rich media
+   * Usually contains code that loads a media player for Youtube videos etc.
+   *
+   * Should not be editable by users as it is directly used and can't be sanitized as
+   * it would remove the integration code/iframe
+   */
+  html?: string;
+  /**
+   * Contains automatic page description extracted
+   * Used to be displayed in fancy link card, or anywhere the original website description is needed
+   */
+  description?: string;
 };
 export interface LinkItemExtra {
   [ItemType.LINK]: LinkItemExtraProperties;
