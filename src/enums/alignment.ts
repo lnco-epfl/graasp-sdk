6d6ec7ef0514d2abf710329f5e8a852e4de4d5fb
Basile Spaenlehauer <spaenleh@gmail.com>
2024-06-28 16:08:41 +0200

    fix: remove UI functions (#569)

--- src/enums/alignment.ts
+++ src/enums/alignment.ts
@@ -6,18 +6,3 @@ export const Alignment = {
   Right: 'right',
 } as const;
 export type AlignmentType = UnionOfConst<typeof Alignment>;
-
-export const DEFAULT_FILE_ALIGNMENT_SETTING = Alignment.Center;
-
-export const getAlignItemsFromAlignmentSetting = (
-  alignment: AlignmentType = DEFAULT_FILE_ALIGNMENT_SETTING,
-): 'flex-start' | 'flex-end' | 'center' => {
-  switch (alignment) {
-    case Alignment.Right:
-      return 'flex-end';
-    case Alignment.Left:
-      return 'flex-start';
-    case Alignment.Center:
-      return 'center';
-  }
-};
